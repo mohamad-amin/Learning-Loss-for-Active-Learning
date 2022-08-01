29c859d64248dc11298672260a314ac6dd83f686
mohamad-amin <mohammadi.mohamadamin@gmail.com>
2022-08-01 03:07:03 -0700

    Reverting back a bit

--- models/wide_resnet.py
+++ models/wide_resnet.py
@@ -21,18 +21,18 @@ def conv_init(m):
 class wide_basic(nn.Module):
     def __init__(self, in_planes, planes, dropout_rate, stride=1, norm_layer=None):
         super(wide_basic, self).__init__()
-        if norm_layer is None:
-            # self.bn1 = nn.BatchNorm2d(in_planes)
-            self.bn1 = nn.BatchNorm2d(in_planes, track_running_stats=True, momentum=0.1)  # also check with momentum=1.0
-        else:
-            self.bn1 = norm_layer(num_channels=in_planes)
+        # if norm_layer is None:
+        #     # self.bn1 = nn.BatchNorm2d(in_planes)
+        #     self.bn1 = nn.BatchNorm2d(in_planes, track_running_stats=True, momentum=0.1)  # also check with momentum=1.0
+        # else:
+        #     self.bn1 = norm_layer(num_channels=in_planes)
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=1, padding=1, bias=True)
         # self.dropout = nn.Dropout(p=dropout_rate)
 
-        if norm_layer is None:
-            self.bn2 = nn.BatchNorm2d(planes, track_running_stats=True, momentum=0.1)
-        else:
-            self.bn2 = norm_layer(num_channels=planes)
+        # if norm_layer is None:
+        #     self.bn2 = nn.BatchNorm2d(planes, track_running_stats=True, momentum=0.1)
+        # else:
+        #     self.bn2 = norm_layer(num_channels=planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
 
         self.shortcut = None
@@ -40,11 +40,11 @@ class wide_basic(nn.Module):
             self.shortcut = nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True)
 
     def forward(self, x):
-        out = self.conv1(F.relu(self.bn1(x)))
-        # out = self.conv1(x)
+        # out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv1(x)
         #out = self.dropout(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        # out = self.conv2(out)
+        # out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv2(out)
         if self.shortcut is not None:
             out += self.shortcut(x)
         return out
@@ -54,6 +54,7 @@ class Wide_ResNet(nn.Module):
     def __init__(self, num_layers, depth, widen_factor, dropout_rate, num_classes,
                  num_input_channels=3, norm_layer=None):
         super(Wide_ResNet, self).__init__()
+        self.in_planes = 16
         self.num_layers = num_layers
 
         assert ((depth-4)%6 ==0), 'wide-resnet depth should be 6n+4'
@@ -62,7 +63,6 @@ class Wide_ResNet(nn.Module):
 
         print('| wide-resnet %dx%d' %(depth, k))
         nstages = [16, 16*k, 32*k, 64*k]
-        self.in_planes = nstages[0]
 
         self.conv1 = conv3x3(num_input_channels, nstages[0])
         if self.num_layers >= 1:
@@ -71,10 +71,10 @@ class Wide_ResNet(nn.Module):
             self.layer2 = self._wide_layer(wide_basic, nstages[2], n, dropout_rate, stride=2, norm_layer=norm_layer)
         if self.num_layers == 3:
             self.layer3 = self._wide_layer(wide_basic, nstages[3], n, dropout_rate, stride=2, norm_layer=norm_layer)
-        if norm_layer is None:
-            self.bn1 = nn.BatchNorm2d(nstages[num_layers], track_running_stats=True, momentum=0.1)
-        else:
-            self.bn1 = norm_layer(num_channels=nstages[num_layers])
+        # if norm_layer is None:
+        #     self.bn1 = nn.BatchNorm2d(nstages[num_layers], track_running_stats=True, momentum=0.1)
+        # else:
+        #     self.bn1 = norm_layer(num_channels=nstages[num_layers])
         self.linear = nn.Linear(nstages[num_layers], num_classes)
 
     def _wide_layer(self, block, planes, num_blocks, dropout_rate, stride, norm_layer=None):
